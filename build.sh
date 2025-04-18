c859d4c471a4d01a404b376fd20830dce3417ca8
Il jo Yoo <iljo.joseph.yoo@gmail.com>
2025-04-18 17:45:29 +0900

    Fixed Arch Errors

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
-python -m venv .venv
+arch -arm64 python3 -m venv .venv
 source .venv/bin/activate
 pip install poetry --index-url http://repo.hyundaicapital.com:8443/repository/pypi-public/simple/
-poetry source add fossera http://repo.hyundaicapital.com:8443/repository/pypi-public/simple/
 poetry install
